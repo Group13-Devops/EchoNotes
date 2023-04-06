9ceececac970ccc3cc611277950c024300216f27
ElizabethHauroo <eliza-hauroo13@hotmail.com>
2023-04-06 17:38:12 +0100

    added hard reset

--- deploy.sh
+++ deploy.sh
@@ -20,9 +20,7 @@ rm -rf NotesAppGroup13/
 # Clone the latest version of the repository
 git clone https://github.com/Group13-Devops/NotesAppGroup13.git
 
-#Pull most recent main branch 
-git reset --hard origin/main
-git pull origin main
+
 
 # Change to the repository folder
 cd NotesAppGroup13/
@@ -30,7 +28,9 @@ echo "Latest commit:"
 git log -1 --oneline
 
 #source update.sh
-
+#Pull most recent main branch 
+git reset --hard origin/main
+git pull origin main
 #############################
 # Install npm dependencies
 npm install
@@ -40,5 +40,5 @@ echo $PRIVATE_KEY > privatekey.pem
 echo $SERVER > server.crt
 
 # Start (or restart) the application with pm2
-pm2 start ./bin/www --name GroupThirteen
-#node app.js
+#pm2 start ./bin/www --name GroupThirteen
+node app.js
